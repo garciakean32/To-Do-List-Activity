9130d8af15f7b6521ff3d5d950ae350df80c3ad3
Kean Valgere E. Garcia <110815853+garciakean32@users.noreply.github.com>
2023-06-03 07:47:27 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,64 @@
-const addBtnClick = document.getElementById("add-btn");
-const container = document.getElementsByClassName("to-do-list-container");
-addBtnClick.addEventListener("click", function() {
-    
-    var newDiv = $("<div id='list' class='added-list-container'><div><p class='added-list'>placeholder</p></div><div class='done-delete-btn'><button class='done-btn'>Done</button><button class='delete-btn'>Delete</button></div></div>");
+const input = document.getElementById("inputBar");
+let toDoList = []
 
-    container.append(newDiv);
+input.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("addBtn").click();
+    }
+});
+
+document.getElementById("addBtn").addEventListener("click", function () {
+
+    if (input.value === "")
+        return alert("Please enter a task");
+
+    if (toDoList.find(x => x === input.value))
+        return alert(`Task "${input.value}" already exist`);
+
+    toDoList.push(input.value)
+
+    const mainContainer = document.getElementById("toDoList");
+    const addedListContainerDiv = document.createElement("div");
+    addedListContainerDiv.setAttribute('class', 'added-list-container')
+
+    mainContainer.appendChild(addedListContainerDiv);
+
+    const divContainter = document.createElement("div");
+    addedListContainerDiv.appendChild(divContainter);
+
+    const p = document.createElement("p");
+    p.setAttribute('class', 'added-list')
+    p.textContent = input.value;
+    divContainter.appendChild(p);
+
+    const divActionContainer = document.createElement("div");
+    divActionContainer.setAttribute('class', 'done-delete-btn')
+    addedListContainerDiv.appendChild(divActionContainer);
+
+    const btnDone = document.createElement("button");
+    btnDone.setAttribute('class', 'done-btn')
+    btnDone.textContent = "Done";
+    divActionContainer.appendChild(btnDone);
+
+    const btnDelete = document.createElement("button");
+    btnDelete.setAttribute('class', 'delete-btn')
+    btnDelete.textContent = "Delete";
+    divActionContainer.appendChild(btnDelete);
+
+    btnDone.addEventListener("click", function () {
+        btnDone.classList.toggle("done-btn-toggle");
+        p.classList.toggle("added-list-toggle");
+
+        btnDone.textContent = btnDone.textContent === "Done" ? "Undone" : "Done"
+
+    });
+
+    btnDelete.addEventListener("click", function () {
+        mainContainer.removeChild(addedListContainerDiv);
+        const inputValue = p.textContent
+        toDoList = toDoList.filter(x => x !== inputValue)
+    });
+
+    input.value = ""
 });
